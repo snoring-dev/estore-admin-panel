c804c1cfb43eb39849c96e579b9d7a703ee5178d
snoring-dev <jemmoudi.med@proton.me>
2023-08-01 23:53:32 +0200

    feat(app): adding type definition for env variables

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const envVariables = z.object({
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+  CLERK_SECRET_KEY: z.string(),
+  NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string(),
+  NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string(),
+  NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string(),
+  NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string(),
+  NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z.string(),
+  DATABASE_URL: z.string(),
+});
+
+envVariables.parse(process.env);
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envVariables> {}
+  }
+}
